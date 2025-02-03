ed49e90f9832a86f12827ffeec0c491cba34d804
Egor Taranov <dev@taranovegor.com>
2025-02-03 17:44:23 +0100

    Add vc.ru and dtf.ru integrations support

---
+++ parser/cmtt/parser.py
@@ -0,0 +1,64 @@
+import requests
+import re
+import json
+
+from datetime import datetime
+
+from core.parser import UnableToParse, Text, Link
+from core.parser import Parser as BaseParser
+
+
+class Parser(BaseParser):
+    URL_REGEX = re.compile(r'^https://(?P<domain>dtf\.ru|vc\.ru)/[^?]+\?comment=(?P<comment_id>\d+)')
+    REACTIONS_EMOJIS = {1: '❤️', 2: '🔥', 22: '😎', 4: '😂', 7: '😱', 3: '🥲', 5: '😡', 9: '🍿', 24: '👀', 10: '💸', 23: '😐', 41: '💊'}
+
+    def __init__(self, user_agent: str):
+        self.user_agent = user_agent
+
+    def supports(self, url):
+        return bool(self.URL_REGEX.match(url))
+
+    def parse(self, string: str) -> Text:
+        match = self.URL_REGEX.search(string)
+        if not match:
+            raise UnableToParse('unsupported scheme')
+
+        domain = match.group('domain')
+        comment_id = int(match.group('comment_id'))
+
+        comments = self.__fetch(f'https://api.{domain}/v2.5/comments?commentId={comment_id}')
+        comment = None
+        for c in comments['result']['items']:
+            if c['id'] == comment_id:
+                comment = c
+                break
+
+        if comment is None:
+            raise UnableToParse('comment not found in response')
+
+        author_id = comment['author']['id']
+        author_name = comment['author']['name']
+        article_id = comment['entry']['id']
+        article_title = comment['entry']['title']
+
+        reactions = []
+        for reaction in comment['reactions']['counters']:
+            r_id = reaction['id']
+            r_count = reaction['count']
+            if r_count > 0 and r_id in self.REACTIONS_EMOJIS:
+                reactions.append(f'{self.REACTIONS_EMOJIS[r_id]} {r_count}')
+
+        return Text(
+            author=Link(url=f'https://{domain}/u/{author_id}/', text=author_name),
+            created_at=datetime.fromtimestamp(comment['date']),
+            content=comment['text'],
+            metrics=reactions,
+            backlink=Link(url=f'https://{domain}/{article_id}?comment={comment_id}', text=article_title),
+        )
+
+    def __fetch(self, url):
+        response = requests.get(url, headers={"User-Agent": self.user_agent})
+        if response.status_code == 200:
+            return json.loads(response.text)
+        else:
+            raise UnableToParse(f'invalid status code {response.status_code}')
