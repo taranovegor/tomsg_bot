1b41825fe980cf9498c00203ca236770f27a03d4
Egor Taranov <dev@taranovegor.com>
2025-02-06 12:15:09 +0100

    Add OK Clips integrations support

--- parser/vk/parser.py
+++ parser/vk/parser.py
@@ -7,18 +7,21 @@ from parser.instagram.meta import HTMLMetaExtractor
 
 
 class Parser(BaseParser):
-    URL_REGEX = re.compile(r'https?://vk\.com/clip-?(\d+)_(\d+)')
+    VK_URL_REGEX = re.compile(r'https?://vk\.com/clip-?(\d+)_(\d+)')
+    OK_URL_REGEX = re.compile(r"https?://ok\.ru/clip\?owner_id=(-?\d+)&clip_id=(\d+)")
 
     def __init__(self, thumbnail_url: str, user_agent: str):
         self.thumbnail_url = thumbnail_url
         self.user_agent = user_agent
 
     def supports(self, url: str) -> bool:
-        return bool(self.URL_REGEX.match(url))
+        return any(pattern.match(url) for pattern in [
+            self.VK_URL_REGEX,
+            self.OK_URL_REGEX,
+        ])
 
     def parse(self, url: str) -> Video:
-        match = self.URL_REGEX.search(url)
-        if not match:
+        if not self.supports(url):
             raise UnableToParse('Unsupported url')
 
         response = requests.get(url, headers={'User-Agent': self.user_agent})
@@ -29,9 +32,14 @@ class Parser(BaseParser):
         if 'og:video' not in meta:
             raise UnableToParse('Failed to retrieve video resource')
 
+        if self.VK_URL_REGEX.match(url):
+            backlink_url = meta.get('og:url')
+        else:
+            backlink_url = url
+
         return Video(
             resource_url=meta.get('og:video'),
             mime_type='video/mp4',
             thumbnail_url=self.thumbnail_url,
-            backlink=Link(meta.get('og:url'), ''),
+            backlink=Link(backlink_url, ''),
         )
