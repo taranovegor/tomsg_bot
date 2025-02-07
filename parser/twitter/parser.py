73fe7532d815643672d0b5105ef79b71cf5352c5
Egor Taranov <dev@taranovegor.com>
2025-02-07 16:18:42 +0100

    Improve the Twitter (X) status parser

--- parser/twitter/parser.py
+++ parser/twitter/parser.py
@@ -1,6 +1,5 @@
 import re
 from datetime import datetime
-from urllib.parse import unquote
 
 import requests
 
@@ -11,61 +10,99 @@ from core import (
     Video,
     ParseError,
     Link,
-    HTMLMetaExtractor,
+    Photo,
 )
 
 
 class Parser(BaseParser):
-    URL_REGEX = re.compile(r'https?://(?:x\.com|twitter\.com)/(?P<username>[^/]+)/status/(?P<status_id>\d+)')
+    """Parser for Twitter URLs to extract tweet information."""
 
-    def __init__(self, video_resource_url: str, user_agent: str):
-        self.video_resource_url = video_resource_url
+    URL_REGEX = re.compile(
+        r"https?://(?:x\.com|twitter\.com)/"
+        r"(?P<username>[^/]+)/status/(?P<status_id>\d+)"
+    )
+
+    def __init__(self, user_agent: str):
+        """Initializes the parser with a user agent for making requests."""
         self.user_agent = user_agent
 
     def supports(self, url: str) -> bool:
+        """Checks if the URL is supported by this parser."""
         return bool(self.URL_REGEX.match(url))
 
     def parse(self, url: str) -> Entity:
+        """Parses the provided Twitter URL and returns an Entity representing the tweet."""
         match = self.URL_REGEX.search(url)
         if not match:
-            raise ParseError('Unsupported url')
+            raise ParseError("Unsupported url")
 
-        username = match.group('username')
-        status_id = match.group('status_id')
+        status_id = match.group("status_id")
 
         response = requests.get(
-            self.video_resource_url.format(username, status_id),
-            headers={'User-Agent': self.user_agent},
+            f"https://api.fxtwitter.com/status/{status_id}",
+            headers={"User-Agent": self.user_agent},
         )
         if response.status_code != 200:
-            raise ParseError('Unhandled response error')
+            raise ParseError("Unhandled response error")
 
-        meta = HTMLMetaExtractor(response.text).extract()
+        json = response.json()
+        if json["code"] != 200:
+            raise ParseError("Unhandled response error")
 
+        tweet = json["tweet"]
+
+        # Create author link and clean up author name
         author = Link(
-            f'https://x.com/{meta.get('twitter:creator')}',
-            re.sub(r'\s*\(@[^)]+\)', '', meta.get('twitter:title')),
+            f"https://x.com/{tweet['author']['screen_name']}",
+            re.sub(r"\s*\(@[^)]+\)", "", tweet["author"]["name"]),
         )
-        content = meta.get('og:description').replace('<br>', '\n')
-        backlink = Link(f'https://x.com/{username}/status/{status_id}', '')
-        if 'article:published_time' in meta:
-            created_at = datetime.fromisoformat(meta.get('article:published_time'))
-        else:
-            created_at = None
+
+        content = tweet["text"] if "text" in tweet else ""
+        metrics = [
+            f"💬 {self.format_counter(tweet['replies'])}",
+            f"🔁 {self.format_counter(tweet['retweets'])}",
+            f"❤️ {self.format_counter(tweet['likes'])}",
+            f"📊 {self.format_counter(tweet['views'])}",
+        ]
+
+        created_at = datetime.strptime(
+            tweet["created_at"],
+            "%a %b %d %H:%M:%S %z %Y",
+        )
+
+        backlink = Link(
+            f"https://x.com/{tweet['author']['screen_name']}/status/{status_id}"
+        )
 
         media = []
-        if 'og:video' in meta:
-            media.append(Video(
-                resource_url=unquote(meta.get('og:video')),
-                mime_type=meta.get('og:video:type'),
-                thumbnail_url=meta.get('og:image'),
-            ))
+        if "media" in tweet:
+            for item in tweet["media"]["all"]:
+                match item["type"]:
+                    case "photo":
+                        media.append(Photo(resource_url=item["url"]))
+                    case "video" | "gif":
+                        media.append(
+                            Video(
+                                resource_url=item["url"],
+                                mime_type="video/mp4",
+                                thumbnail_url=item["thumbnail_url"],
+                            )
+                        )
 
         return Content(
             author=author,
             created_at=created_at,
-            metrics=[],
+            metrics=metrics,
             text=content,
             backlink=backlink,
             media=media,
         )
+
+    @staticmethod
+    def format_counter(number):
+        """Formats a number into a human-readable counter (e.g., 1K, 1M)."""
+        if number >= 1_000_000:
+            return f"{number / 1_000_000:.0f}M"
+        if number >= 1_000:
+            return f"{number / 1_000:.0f}K"
+        return str(number)
